91c485912333912a0ed82669da3ec5b2fbfd2a6b
Crowdbotics <team@crowdbotics.com>
2022-03-11 21:14:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/karter_34134/MainActivity.java
@@ -0,0 +1,15 @@
+package com.karter_34134;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "karter_34134";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "karter_34134",
+  url: "https://karter_34134.botics.co",
+  api: "https://karter_34134.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
